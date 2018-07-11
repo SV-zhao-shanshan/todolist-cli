a59d209c9cbae8a0ab86c732a86065adc533af6b
Shan Shan Zhao <shsszhao@cn.ibm.com>
2018-07-11 16:34:22 +0800

    learn how to use vue-cli

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <li @click="handleClick">
+    {{content}}
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: [ 'content', 'index' ],
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    handleClick () {
+      this.$emit('delete', this.index)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
